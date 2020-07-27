9bba99e1febbb6d28a733e142e3e2ea7194201d2
arnoldgihozo <gihozo@ualberta.ca>
2020-07-27 17:55:17 -0600

    refactored the sign up page to work with global keys

--- ios/Flutter/flutter_export_environment 2.sh
+++ ios/Flutter/flutter_export_environment 2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/arnoldgihozo/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/arnoldgihozo/Library/Mobile Documents/com~apple~CloudDocs/My projects/pint_size"
-export "FLUTTER_TARGET=/Users/arnoldgihozo/Library/Mobile Documents/com~apple~CloudDocs/My projects/pint_size/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/arnoldgihozo/Desktop/My projects/pint_size"
+export "FLUTTER_TARGET=/Users/arnoldgihozo/Desktop/My projects/pint_size/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
